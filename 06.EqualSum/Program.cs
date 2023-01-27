4230779bd2175fa22d754f16d255ec6d5e7c68ca
Bobby-PC <Bobby-PC@BOBBY-PC>
2023-01-27 21:11:16 +0200

    Add project files.

---
+++ 06.EqualSum/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] array = Console.ReadLine()
+    .Split(' ')
+    .Select(int.Parse)
+    .ToArray();
+
+for(int i = 0;i < array.Length; i++)
+{
+    int leftsum = 0;
+    int rightsum = 0;
+
+    for (int j = 0; j < i; j++)
+    {
+        leftsum += array[j];
+    }
+    for (int j = i+1; j < array.Length; j++)
+    {
+        rightsum += array[j];
+    }
+    if (leftsum == rightsum)
+    {
+        Console.WriteLine(i);
+        return;
+    }
+}
+Console.WriteLine("no");
